2d74f2df3c577669eaab40fe9452a722aa68c2ff
Jonatan Hägglund <jonatan.hagglund@hiq.se>
2017-05-17 23:15:52 +0200

    First commit from command line with some random changes

--- app/src/main/java/com/example/jonte/wifitruckcontroller/MainActivity.java
+++ app/src/main/java/com/example/jonte/wifitruckcontroller/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.jonte.wifitruckcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String appNameStr;
+    private TextView mainTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appNameStr = getString(R.string.app_name);
+        mainTextView = (TextView)findViewById(R.id.mainTextView);
+        mainTextView.setText(mainTextView.getText() + "\n" + appNameStr);
     }
+
+
 }
